06938129a0795df86cdc7f48c25b0c4b73eb80a3
Ahmed Tanwar <ahmedtanwar017@gmail.com>
2025-09-18 09:59:53 +0530

    Completeion

--- backend/routes/adminsRouter.js
+++ backend/routes/adminsRouter.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { loginAdmin, getAdminDashboard } = require("../controllers/adminController");
-// const { isAdmin } = require("../middlewares/authMiddleware");
+const { isAdmin } = require("../middlewares/authMiddleware");
 
 // Admin login
 router.post("/auth/login", loginAdmin);
 
 // Admin dashboard (protected)
-router.get("/admin/dashboard",  getAdminDashboard);
+router.get("/admin/dashboard", isAdmin, getAdminDashboard);
 
 module.exports = router;
